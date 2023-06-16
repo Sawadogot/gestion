fb784752c43ead7e94f296ac99c089d3393dc264
Sawadogot <127124388+Sawadogot@users.noreply.github.com>
2023-06-16 15:12:00 +0000

    Add files via upload

--- administrateur.php
+++ administrateur.php
@@ -9,28 +9,34 @@
 </head>
 <body>
     <div class="container">
-            <div class="main">  	
-                <input type="checkbox" id="chk" aria-hidden="true">
-
-                <div class="login">
-                    <form class="form">
-                        <label for="chk" aria-hidden="true">Log in</label>
-                        <input class="input" type="email" name="email" placeholder="Email" required="">
-                        <input class="input" type="password" name="pswd" placeholder="Password" required="">
-                        <button>Log in</button>
-                    </form>
-                </div>
-
-                <div class="register">
-                    <form class="form">
-                        <label for="chk" aria-hidden="true">Register</label>
-                        <input class="input" type="text" name="txt" placeholder="Username" required="">
-                        <input class="input" type="email" name="email" placeholder="Email" required="">
-                        <input class="input" type="password" name="pswd" placeholder="Password" required="">
-                        <button>Register</button>
-                    </form>
-                </div>
-            </div>
+    <div class="login-box">
+ <form>
+    <div>
+    <img class="logo" src="images/MySchool.png" alt="" heigth="120" width="120">
+    </div>
+    <div>
+        <h2 class="ad">
+        INSCRIPTION-ADMIN
+        </h2>
+    </div>
+   <div class="user-box">
+     <input type="text" name="" required="">
+     <label>Nom complet</label>
+   </div>
+   <div class="user-box">
+     <input type="text" name="" required="">
+     <label>E-mail</label>
+   </div>
+   <div class="user-box">
+     <input type="password" name="" required="">
+     <label>Password</label>
+   </div><center>
+   <a href="login.php">
+    Se connecter
+      <span></span>
+   </a></center>
+ </form>
+</div>
     </div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styl.css">
+    <title>login</title>
+</head>
+<body>
+    <div class="container">
+    <div class="login-box">
+    
+ <form>
+    <div>
+    <img class="logo" src="images/MySchool.png" alt="" heigth="120" width="120">
+    </div>
+   <div class="user-box">
+     <input type="text" name="" required="">
+     <label>E-mail</label>
+   </div>
+   <div class="user-box">
+     <input type="password" name="" required="">
+     <label>Password</label>
+   </div><center>
+   <a href="#">
+          Se connecter
+      <span></span>
+   </a></center>
+ </form>
+</div>
+    </div>
+</body>
+</html>
